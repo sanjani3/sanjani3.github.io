b365950cd5f3f460c82f81e68b0c2d3b843833dd
20WH1A0586 <94345379+sanjani3@users.noreply.github.com>
2023-06-07 16:46:37 +0530

    added the new user card

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("HEY!!")
+var data1 = {image : "Recentimage.jpg",name : "P.Renusree" , designation: "Student"};
+var data2 = {image : "ChildhoodImage.jpg",name : "Renu" , designation: "student"};
+var isdata1 = true;
+var displaydata;
+var changemypic = function(){
+    if(isdata1){
+        displaydata = data2;
+        isdata1=false;
+    }
+    else{
+        displaydata = data1;
+        isdata1 = true;
+    }
+    document.getElementById("myimage").src=displaydata.image;
+    document.getElementById("name2").innerHTML=displaydata.name;
+    document.getElementById("designation2").innerHTML=displaydata.designation;
+
+}
